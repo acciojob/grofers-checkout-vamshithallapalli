6ebb9fca252a29dc5b5722d19d8a66f8601211cb
Vamshi Thallapalli <127075912+vamshithallapalli@users.noreply.github.com>
2025-11-05 13:08:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,22 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+ const priceCells = document.querySelectorAll('.price');
+
+	let total = 0;
+	priceCells.forEach( cell => {
+		total += Number(cell.textContent);
+	})
+
+	const newRow = document.createElement('tr');
+	const newCell = document.createElement('td');
+
+	newCell.setAttribute('colspan', '2');
+	newCell.textContent = total;
+
+	newRow.appendChild(newCell);
+
+	document.querySelector("table").appendChild(newRow);
   
 };
 
